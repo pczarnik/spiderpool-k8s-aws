f8dae9c4a2fdec89f1b15989d5388054b59aee2b
Piotr Czarnik <ptr.czarnik@gmail.com>
2024-06-06 15:05:47 +0200

    Rename playbooks

--- run.sh
+++ run.sh
@@ -4,13 +4,12 @@ set -eu
 set -o pipefail
 
 cd terraform
-terraform apply --auto-approve
+# terraform apply --auto-approve
 cd ..
 
 cd ansible
-sleep 10
-ansible-playbook -i inventory/hosts.cfg predeploy.yml
-ansible-playbook -i inventory/hosts.cfg deploy_k8s.yml
-ansible-playbook -i inventory/hosts.cfg deploy_spiderpool.yml
-ansible-playbook -i inventory/hosts.cfg deploy_apps.yml
+# ansible-playbook -i inventory/hosts.cfg pre-deploy.yml
+ansible-playbook -i inventory/hosts.cfg deploy-k8s.yml
+ansible-playbook -i inventory/hosts.cfg deploy-spiderpool.yml
+ansible-playbook -i inventory/hosts.cfg deploy-apps.yml
 cd ..
